ec45be14ab7be2e636314077c0f0d7f86de8f45a
Aman <patilaman080@gmail.com>
2021-05-31 01:08:37 +0530

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log('connected')
+
+const Check=()=>{
+    const checkBox = document.querySelector('#checbox')
+    const text1=document.querySelector('.text1')
+    const text2=document.querySelector('.text2')
+    const text3=document.querySelector('.text3')
+
+    console.log(text1,text2,text3)
+
+    if(checkBox.checked == true){
+        text1.innerHTML=`$${19.99}`
+        text2.innerHTML=`$${24.99}`
+        text3.innerHTML=`$${39.99}`
+
+    }else{
+        text1.innerHTML=`$${199.99}`
+        text2.innerHTML=`$${249.99}`
+        text3.innerHTML=`$${399.99}`
+
+
+
+    }
+
+    
+
+}
